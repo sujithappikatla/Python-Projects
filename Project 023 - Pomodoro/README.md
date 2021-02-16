31d4aab67a08cecbdf51a2377cf5e9838fa072ca
sujith <sujith.appikatla@gmail.com>
2021-02-17 00:30:24 +0530

    Add - P23 Pomodora

---
+++ Project 023 - Pomodoro/README.md
@@ -0,0 +1,16 @@
+# Pomodora
+
+**Level -** Medium | Beginner
+
+**Modules Used -** Tkinter
+
+**Description -** A GUI Application for Pomodoro Technique. The **Pomodoro Technique** is a time management **method** developed by Francesco Cirillo in the late 1980s. The **technique** uses a timer to break down work into intervals, traditionally 25 minutes in length, separated by short breaks.
+
+![Image Explaining Pomodoro Technique](https://images.ctfassets.net/dm4oa8qtogq0/390glBwOnV44EyoPoiJC6Z/3ddbb50dbc61e3a11afc6b05aa9e21ab/productivity-method_pomodoro-summary.jpg)
+
+**Compilation -** python main.py
+
+![Preview GIF](./preview.gif)
+
+
+
---
+++ Project 023 - Pomodoro/main.py
@@ -0,0 +1,107 @@
+from tkinter import *
+import math
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+
+def reset():
+    """Reset Timer, check marks and title"""
+    global reps
+    reps = 0
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text=f"00:00")
+    label.config(text="TIMER")
+    check_label.config(text="")
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+
+def start_timer():
+    """Starts countdown"""
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        label.config(text="BREAK", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        label.config(text="BREAK", fg=PINK)
+        count_down(short_break_sec)
+    else:
+        label.config(text="WORK", fg=GREEN)
+        count_down(work_sec)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def count_down(count):
+    """CountDown the sessions"""
+    minutes = math.floor(count / 60)
+    seconds = count % 60
+    if seconds < 10:
+        seconds = f"0{seconds}"
+
+    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count-1)
+    else:
+        start_timer()
+        marks = "✔" * math.floor(reps / 2)
+        check_label.config(text=marks)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title("Pomodora")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+# label
+label = Label(text="TIMER", font=(FONT_NAME, 50, "normal"))
+label.config(bg=YELLOW, fg=GREEN)
+label.grid(row=0, column=1)
+
+# tomato timer
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+# start button
+start_button = Button(text="START", highlightthickness=0)
+start_button.config(command=start_timer)
+start_button.grid(row=2, column=0)
+
+# Reset button
+reset_button = Button(text="RESET", highlightthickness=0)
+reset_button.config(command=reset)
+reset_button.grid(row=2, column=3)
+
+# ✔ label
+check_label = Label(text="", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 16))
+check_label.grid(row=3, column=1)
+
+
+window.mainloop()
